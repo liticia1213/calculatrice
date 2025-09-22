b65b61a45b58171b4d8a0ea81c45d8870f7c109a
liticia idjoubar <liticiaidjoubar@gmail.com>
2025-09-22 19:40:13 +0100

    commit

---
+++ code.js
@@ -0,0 +1,11 @@
+function add(num){
+if(document.getElementById("result").textContent=="0"){
+    document.getElementById("result").textContent=num;
+}else{
+    document.getElementById("result").textContent=document.getElementById("result").textContent+num;
+}
+}
+function addAdition(){
+    
+document.getElementById("result").textContent + '+'
+}
